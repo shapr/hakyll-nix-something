3580f4a0115690e96fc97aedeb7c6647731f25a9
Shae Erisson <shae@scannedinavian.com>
2023-09-22 15:41:06 -0500

    flake-compat wrap this in default.nix for server happiness

--- flake.nix
+++ flake.nix
@@ -17,8 +17,12 @@
   inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
   inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flake-compat = {
+    url = "github:edolstra/flake-compat";
+    flake = false;
+  };
 
-  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+  outputs = { self, nixpkgs, flake-utils, haskellNix, flake-compat }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ haskellNix.overlay
